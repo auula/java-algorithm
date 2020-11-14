4c1162b2bfbffcf36bce8cfdd30af1825b7fcceb
Dings <deen.job@qq.com>
2020-11-14 21:36:00 +0800

    add quick sort impl.

--- src/main/java/me/ibyte/algorithm/sort/QuickSort.java
+++ src/main/java/me/ibyte/algorithm/sort/QuickSort.java
@@ -4,32 +4,34 @@ package me.ibyte.algorithm.sort;
 import me.ibyte.algorithm.Sort;
 
 
-
+// https://www.bilibili.com/video/BV1at411T75o?zw
 public class QuickSort implements Sort {
     @Override
     public Integer[] sort(Integer[] arr) {
-        return quickSort(arr,0,arr.length-1);
+        return quickSort(arr, 0, arr.length - 1);
     }
 
-    private  Integer[] quickSort(Integer[] arr, int left, int right) {
+    private Integer[] quickSort(Integer[] arr, int left, int right) {
         if (left >= right) {
             return arr;
         }
-        int pivot = arr[left],L=left,R=right;
-        while(left < right){
-            while (left < right && arr[right] >= pivot){
+        // L记录开始位置 R记录尾巴位置
+        int pivot = arr[left], L = left, R = right;
+        while (left < right) {
+            while (left < right && arr[right] >= pivot) {
                 right--;
             }
             arr[left] = arr[right];
-            while(left < right && arr[left] <= pivot){
+            while (left < right && arr[left] <= pivot) {
                 left++;
             }
             arr[right] = arr[left];
         }
         arr[left] = pivot;
-        quickSort(arr,L,right-1);
-        quickSort(arr,right+1,R);
-        return  arr;
+        // L =0 left = 是中心轴值的位置 -1 分左右作用
+        quickSort(arr, L, left - 1);
+        quickSort(arr, left + 1, R);
+        return arr;
     }
 
 
